fc69dcc5ce11dd27743d31781d3b24aea85b3d34
ks603 <kevin.shaughnessy18@gmail.com>
2020-07-27 12:26:07 -0500

    Created darkmode function to change text and icons

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]')
+const nav = document.getElementById('nav')
+const toggleIcon = document.getElementById('toggle-icon')
+const image1 = document.getElementById('image1')
+const image2 = document.getElementById('image2')
+const image3 = document.getElementById('image3')
+const textBox = document.getElementById('text-box')
+
+// Dark Mode Styles
+
+const darkMode = () => {
+  nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+  textBox.style.backgoundColor = 'rgb(255 255 255 / 50%)'
+  toggleIcon.children[0].textContent = 'Dark Mode'
+  toggleIcon.children[1].classList.remove('fa-sun')
+  toggleIcon.children[1].classList.add('fa-moon')
+}
 
 // Switch Theme
 
 const switchTheme = (event) => {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark')
+    darkMode()
   } else {
     document.documentElement.setAttribute('data-them', 'light')
+    lightMode()
   }
 }
 
