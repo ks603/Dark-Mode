08e196747dd6fd56c1a569ddc15689581893d9f2
ks603 <kevin.shaughnessy18@gmail.com>
2020-07-27 12:55:09 -0500

    Create functions for light and dark mode also stored in local storage

--- script.js
+++ script.js
@@ -8,12 +8,28 @@ const textBox = document.getElementById('text-box')
 
 // Dark Mode Styles
 
+const imageMode = (color) => {
+  image1.src = `img/undraw_proud_coder_${color}.svg`
+  image2.src = `img/undraw_feeling_proud_${color}.svg`
+  image3.src = `img/undraw_conceptual_idea_${color}.svg`
+}
+
 const darkMode = () => {
   nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
   textBox.style.backgoundColor = 'rgb(255 255 255 / 50%)'
   toggleIcon.children[0].textContent = 'Dark Mode'
-  toggleIcon.children[1].classList.remove('fa-sun')
-  toggleIcon.children[1].classList.add('fa-moon')
+  toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon')
+  imageMode('dark')
+}
+
+// Light Mode Styles
+
+const lightMode = () => {
+  nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+  textBox.style.backgoundColor = 'rgb(0 0 0 / 50%)'
+  toggleIcon.children[0].textContent = 'Light Mode'
+  toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+  imageMode('light')
 }
 
 // Switch Theme
@@ -21,12 +37,24 @@ const darkMode = () => {
 const switchTheme = (event) => {
   if (event.target.checked) {
     document.documentElement.setAttribute('data-theme', 'dark')
+    localStorage.setItem('theme', 'dark')
     darkMode()
   } else {
-    document.documentElement.setAttribute('data-them', 'light')
+    document.documentElement.setAttribute('data-theme', 'light')
+    localStorage.setItem('theme', 'light')
     lightMode()
   }
 }
 
 // Event Listener
 toggleSwitch.addEventListener('change', switchTheme)
+
+// Check Local Storage For Theme
+const currentTheme = localStorage.getItem('theme')
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme)
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true
+    darkMode()
+  }
+}
